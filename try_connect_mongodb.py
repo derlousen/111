f24c022056bdc2bf26a567e58ce901c4a9dfb3cf
Pengkiki <pengqi193@gmail.com>
2018-11-27 01:09:19 -0800

    data_test

--- try_connect_mongodb.py
+++ try_connect_mongodb.py
@@ -1,1 +1,18 @@
 # -*- coding: utf-8 -*-
+import pymongo
+
+myclient = pymongo.MongoClient('mongodb://192.168.1.19:12345/')
+
+# dblist = myclient.list_database_names()
+# # dblist = myclient.database_names()
+# if "runoobdb" in dblist:
+#     print("数据库已存在！")
+
+mydb = myclient["record"]
+
+btc = mydb['btc']
+
+
+x = btc.find()[0]
+
+print(x['data'])
